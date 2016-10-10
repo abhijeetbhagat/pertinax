9d3a307cc9fa915c742ba5278bbe070f928590aa
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2016-10-10 09:06:21 -0700

    Add Uri struct

--- src/uri.rs
+++ src/uri.rs
@@ -1,3 +1,17 @@
-pub struct Uri{
+enum Scheme{
+    NamedPipe,
+    Tcp,
+}
 
+pub struct Uri<'a>{
+    scheme : Scheme,
+    host : &'a str,
+    port : &'a str,
+    path : &'a str, 
+}
+
+impl<'a> Uri<'a>{
+    fn new()->Self{
+        unimplemented!();
+    }
 }
